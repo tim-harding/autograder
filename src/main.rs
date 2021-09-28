ea8af8fde840632236e6328ce7e7c640bef34f5a
Tim Harding <Tim@TimHarding.co>
2021-09-28 16:34:45 -0700

    Command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::{Clap};
+
+#[derive(Clap, Debug, Clone, Hash, PartialEq, Eq)]
+struct Options {
+    #[clap(short, long)]
+    config: String,
+    #[clap(short, long)]
+    program: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Options = Options::parse();
 }
