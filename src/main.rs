1699ef198fd3e0fd636b28fcd2d199e665608eb6
Tim Harding <Tim@TimHarding.co>
2021-09-28 18:03:28 -0700

    Reading the config file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{Clap};
+use anyhow::Result;
+use std::fs::File;
+use std::io::BufReader;
+use serde::Deserialize;
 
 #[derive(Clap, Debug, Clone, Hash, PartialEq, Eq)]
 struct Options {
@@ -8,6 +12,37 @@ struct Options {
     program: String,
 }
 
-fn main() {
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+struct ConfigRoot {
+    tests: Vec<TestCase>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+struct TestCase {
+      name: String,
+      setup: Option<String>,
+      run: String,
+      input: String,
+      output: String,
+      comparison: Comparison,
+      timeout: u16,
+      points: Option<u16>,
+}
+
+#[derive(Deserialize, Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[serde(rename_all = "camelCase")]
+enum Comparison {
+    Included,
+    Exact,
+    Regex,
+}
+
+fn main() -> Result<()> {
     let opts: Options = Options::parse();
+    let file = File::open(opts.config)?;
+    let reader = BufReader::new(file);
+    let config: ConfigRoot = serde_json::from_reader(reader)?;
+    Ok(())
 }
