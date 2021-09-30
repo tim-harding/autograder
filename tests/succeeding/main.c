b853b5930139ef16911a6083dbb505a396a7f54e
Tim Harding <tim@timharding.co>
2021-09-30 12:33:36 -0700

    Added tests

---
+++ tests/succeeding/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    char buffer[BUFFER_SIZE];
+    fgets(buffer, BUFFER_SIZE, stdin);
+    int length = strlen(buffer);
+    for (int i = 0; i < length / 2; i++) {
+        char tmp = buffer[i];
+        int end_index = length - i - 1;
+        buffer[i] = buffer[end_index];
+        buffer[end_index] = tmp;
+    }
+    printf("%s\n", buffer);
+}
